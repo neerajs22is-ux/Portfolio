3a0e7be7636548b9e81e5c8ab98225ce934c9730
nee <neeeraj@lab.com>
2026-08-25 00:23:25 +0530

    Background depth: blueprint grid, drifting teal light orbs

--- src/components/MainContainer.tsx
+++ src/components/MainContainer.tsx
@@ -29,6 +29,8 @@ const MainContainer = ({ children }: PropsWithChildren) => {
 
   return (
     <div className="container-main">
+      <div className="bg-orb bg-orb-1" aria-hidden="true"></div>
+      <div className="bg-orb bg-orb-2" aria-hidden="true"></div>
       <Cursor />
       <Navbar />
       <SocialIcons />
